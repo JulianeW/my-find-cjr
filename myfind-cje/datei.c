376a549ba080c72d0fb194ddc2eb2294868667c0
claudia.baierl <claudia.baierl@57052a70-eb47-fa77-b0e1-2620185ca0e1>
2015-02-23 15:15:01 +0000

    fresh project start with .cproject .project etc.

---
+++ myfind-cje/datei.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
+void checkFile(const char *file);
+do_file(const char * file_name, const char * const * parms);
+do_dir(const char * dir_name, const char * const * parms);
+
+
+int main(int argc, char* argv[])
+{
+	/* const char * const *paramlist = (const char * const *)&argv[1];*/
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "zu wenig Argumente..\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("Parameter 1: %s\n", argv[1]);
+	
+	printf("Anzahl der Argumente: %d\n", argc);
+	printf("committest");
+
+	checkFile(argv[1]);
+
+	return EXIT_SUCCESS;
+}
+
+void checkFile(const char *file)
+{
+
+	struct stat mystat;
+	char *error;
+
+	if(lstat(file,&mystat) == -1)
+	{
+	 fprintf(stderr, "mystat() failed..\n");
+	fprintf(stderr, "Fehler: %s\n", strerror(errno));	
+/*error = strerror(errno);
+	perror(error);*/	
+	
+	exit(EXIT_FAILURE);
+	 }
+
+	
+	lstat(file, &mystat);
+
+	
+	 
+	if (S_ISREG(mystat.st_mode)) printf("File: %s\n", file );
+	else if (S_ISDIR(mystat.st_mode)) printf("Directory: %s \n", file);
+
+}
